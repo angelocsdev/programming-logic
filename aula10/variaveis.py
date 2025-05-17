e03cd3035f76887ca216e9bf352a3ec4bb74cdea
Angelo <angelobielcosta123@gmail.com>
2025-05-16 21:13:28 -0300

    Aula 10

---
+++ aula10/variaveis.py
@@ -0,0 +1,17 @@
+# Variáveis
+
+# Números
+
+velocidade_internet =  10 # Variáveis em python não podem começar com número ou símbolos. Uma boa prática é escrever as variáveis em minúsculo e separadas por underline (_).
+print(velocidade_internet)
+
+nota_filme = 8.5 # float
+print(nota_filme)
+
+# Valores boleanos
+
+aberto = True
+
+# Strings
+
+nome = 'Ângelo'
