93957712af83aa218ec073f9ffcd77e05df3d06d
Angelo Silva <angelobielcosta123@gmail.com>
2025-06-04 14:46:43 -0300

    Login test

---
+++ projetos/my-project/login/login.py
@@ -0,0 +1,8 @@
+# Login -> my project
+
+print("Olá, seja bem vindo ao seu gerenciador de finanças pessoais!\nSe essa for sua primeira vez usando o app, sugiro que faça login para manter suas informações salvas na nuvem.")
+
+nome_de_usuario = input("Digite um nome de usuário...")
+e_mail = input("Digite seu melhor e-mail...")
+
+print("Seja bem-vindo(a), " + nome_de_usuario + "! Fico feliz que tenha escolhido dar esse passo importante rumo a independência financeira.")
